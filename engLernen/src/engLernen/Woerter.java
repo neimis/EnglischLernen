df5c9cbbaea1b8abd3aa9b18a9207a508ec916c1
Cezar <Cezar@DESKTOP-RM6L550>
2022-02-23 18:57:23 +0100

    some comments were added

--- engLernen/src/engLernen/Woerter.java
+++ engLernen/src/engLernen/Woerter.java
@@ -2,49 +2,105 @@ package engLernen;
 
 import java.io.Serializable;
 
-/* this class is for creating an word-object  */
-
+/**
+ * this class is for creation an word-object, which implements serializable interface
+ * for reading Woerter Objects from the file
+ */
 
 public class Woerter implements Serializable {
-
+	/**
+	 * german translation
+	 */
 	private String de;
+	/**
+	 * english translation
+	 */
 	private String en;
+	/**
+	 * example sentence in english
+	 */
 	private String detB;
 
+	/**
+	 * getter
+	 * 
+	 * @return - german translation
+	 */
 	public String getDe() {
 		return de;
 	}
 
+	/**
+	 * setter
+	 * 
+	 * @param en - german translation
+	 */
 	private void setDe(String de) {
 		this.de = de;
 	}
 
+	/**
+	 * getter
+	 * 
+	 * @return - english translation
+	 */
 	public String getEn() {
 		return en;
 	}
 
+	/**
+	 * setter
+	 * 
+	 * @param en - english translation
+	 */
 	private void setEn(String en) {
 		this.en = en;
 	}
 
+	/**
+	 * getter
+	 * 
+	 * @return - example sentence in english
+	 */
 	public String getDetB() {
 		return detB;
 	}
 
+	/**
+	 * setter
+	 * 
+	 * @param detB - example sentence in english
+	 */
 	private void setDetB(String detB) {
 		this.detB = detB;
 	}
 
+	/**
+	 * constructor for creating an Woerter object, the following parameters are
+	 * instantiated:
+	 * 
+	 * @param de - german translation
+	 * @param en - english translation
+	 */
 	public Woerter(String de, String en) {
 		this.de = de;
 		this.en = en;
 		this.detB = "Kein Beispiel vorhanden";
 	}
 
-	public Woerter(String de, String en, String detBeschreibung) {
+	/**
+	 * constructor for creating an Woerter object, the following parameters are
+	 * instantiated:
+	 * 
+	 * @param de       - german translation
+	 * @param en       - english translation
+	 * @param detBesch - example sentence in english
+	 */
+
+	public Woerter(String de, String en, String detBesch) {
 		this.de = de;
 		this.en = en;
-		this.detB = detBeschreibung;
+		this.detB = detBesch;
 	}
 
 	public String toString() {
